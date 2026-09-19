6deec523fdb9adcb7ec493e89890df4ce9872f3b
haneesh koushik <f20240931@pilani.bits-pilani.ac.in>
2026-09-19 14:39:18 +0000

    lab02 task4

---
+++ labs/lab02/task4/and_beh_before.v
@@ -0,0 +1,3 @@
+module and_beh_before(input a,b,output reg y);
+  always @(*) #3 y = a & b;
+endmodule
---
+++ labs/lab02/task4/and_beh_intra.v
@@ -0,0 +1,3 @@
+module and_beh_intra(input a,b,output reg y);
+  always @(*) y = #3 a & b;
+endmodule
